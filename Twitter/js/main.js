0c43f307e679dc1dcb2ab2181b3b6844b695d8e1
NatashaDianeey <ndianeey@gamil.com>
2016-10-20 16:55:12 +0000

    sin terminar

---
+++ Twitter/js/main.js
@@ -0,0 +1,67 @@
+// // function agregar() {
+
+
+// // 	var valor = document.getElementById("parrafo").value;
+// // }
+
+// function limita(maximoCaracteres) {
+//   var elemento = document.getElementById("texto").value;
+//   if(elemento.value.length >= maximoCaracteres ) {
+//     return false;
+//   }
+//   else {
+//     return true;
+//   }
+// }
+ 
+function get_lista() {
+    var lista = new Array;
+    var lista_str = localStorage.getItem('todo');
+    if (lista_str !== null) {
+        lista = JSON.parse(lista_str); 
+    }
+    return lista;
+}
+
+function add() {
+    var tarea = document.getElementById('tarea').value;
+
+    var lista = get_lista();
+    lista.push(tarea);
+    localStorage.setItem('todo', JSON.stringify(lista));
+    document.getElementById('tarea').value="";
+    show();
+
+    return false;
+}
+
+function remove() {
+    var id = this.getAttribute('id');
+    var lista = get_lista();
+    lista.splice(id, 1);
+    localStorage.setItem('todo', JSON.stringify(lista));
+
+    show();
+
+    return false;
+}
+
+function show() {
+    var lista = get_lista();
+
+    var html = '<ul>';
+    for(var i=0; i<lista.length; i++) {
+        html += '<li>' + lista[i] + '<button class="remove" id="' + i  + '"><i class="fa fa-trash-o" aria-hidden="true"></i></button></li>';
+    };
+    html += '</ul>';
+
+    document.getElementById('lista').innerHTML = html;
+
+    var buttons = document.getElementsByClassName('remove');
+    for (var i=0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', remove);
+    };
+}
+
+document.getElementById('add').addEventListener('click', add);
+show();
