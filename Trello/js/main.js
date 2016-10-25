5d9652d16faf198b44cb09a912f90a2d8e7c2080
NatashaDianeey <ndianeey@gamil.com>
2016-10-25 16:33:12 +0000

    trello sin terminar

--- Trello/js/main.js
+++ Trello/js/main.js
@@ -1,36 +1,50 @@
-function crearLista (obj) {
+var btnClickInput = document.getElementById('crearLista');
+	btnClickInput.onclick =  crearLista;
 
-	
-	var createForm = document.createElement('form');
-		createForm.setAttribute('id','fondo');
+var crearTarjeta = document.getElementById('crearTarjeta');
 
-	var createInput = document.createElement('input');
-		createInput.setAttribute('id','cajaTexto');
+var createInput = document.createElement('input');
+	createInput.setAttribute('id','cajaTexto');
+function crearLista(){
+
+	var createForm = document.createElement('div');
+		createForm.setAttribute('class','fondo');
 		
 	var createButton = document.createElement('button');
-		createButton.innerHTML="Guardar"
- 	   	createButton.setAttribute('id','botonVerde');
+		createButton.innerHTML="Guardar";
  	   	createButton.setAttribute('class','btn btn-default'); 
+ 	
+ 	var createSpan = document.createElement('span');
+ 		createSpan.setAttribute('class', 'fa fa-close');
 
-
+ 	crearTarjeta.appendChild(createForm);
  	createForm.appendChild(createInput);
  	createForm.appendChild(createButton);
- 	crearTarjeta.appendChild(createForm);
+ 	createForm.appendChild(createSpan);
 
-	
-    obj.innerHTML = (crearTarjeta);
-
-
-
-
-
+	createButton.onclick = guardar;	
 }
 
+function guardar() {
+	if (createInput.value === "") {
+		createInput.setAttribute('placeholder', 'Ingresa texto válido');
+		return false;
+	} 
 
-// var cajaSelectUno = document.getElementById('crearLista');
-// 	cajaSelectUno.onclick = function () {
-// 		crearLista ();
-// 	}
+	var h4 = document.createElement('h4');
+	var guardarTexto =document.createElement('div');
+	guardarTexto.setAttribute('class', 'fondo');
+
+	var inputDentroDiv = document.createElement('input');
+	inputDentroDiv.setAttribute('placeholder', 'Añade una Tarjeta')
+	inputDentroDiv.setAttribute('class', 'inputDentroDivClass')
+
+	crearTarjeta.appendChild(guardarTexto);
+	h4.innerHTML = createInput.value;
+	guardarTexto.appendChild(h4);
+	guardarTexto.appendChild(inputDentroDiv);
 
 
-
+	//Comenzar en 0 el input de Inicio
+	createInput.value = "";
+}
